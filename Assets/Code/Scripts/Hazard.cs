90477dc3b3dbc3b689bd7da995296932e3536bae
lomstfer <90508408+lomstfer@users.noreply.github.com>
2026-02-19 12:39:26 +0100

    fix tag logic

--- Assets/Code/Scripts/Hazard.cs
+++ Assets/Code/Scripts/Hazard.cs
@@ -7,7 +7,7 @@ public class Hazard : MonoBehaviour
     [SerializeField] List<string> tagsToLookFor;
     void OnTriggerEnter2D(Collider2D col)
     {
-        if (tagsToLookFor.Contains(col.tag))
+        if (!tagsToLookFor.Contains(col.tag))
             return;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
