ed077577a48d3a20ede6a52173beafb937345745
lomstfer <gabriel4wingard@gmail.com>
2026-02-20 11:04:11 +0100

    remove player tags from player abilities

--- Assets/Code/Scripts/Abilities/Items/AbilityUnlockItem.cs
+++ Assets/Code/Scripts/Abilities/Items/AbilityUnlockItem.cs
@@ -5,12 +5,11 @@ public class AbilityUnlockItem : MonoBehaviour
     [SerializeField] PlayerAbilityType abilityToUnlock;
     [SerializeField] UnlockedAbilities unlockedAbilities;
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D col)
     {
-        if (other.tag == "Player")
-        {
-            unlockedAbilities.UnlockAbility(abilityToUnlock);
-            Destroy(gameObject); // TODO: animation?
-        }
+        if (!col.CompareTag("Player"))
+            return;
+        unlockedAbilities.UnlockAbility(abilityToUnlock);
+        Destroy(gameObject); // TODO: animation?
     }
 }
