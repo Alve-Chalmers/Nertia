d9cf960f8639819ca69bf6566c6fc86edd398f94
Alve Svarén <alvel@student.chalmers.se>
2026-02-23 11:53:05 +0100

    Fix bug

--- Assets/EmitOutsideCamera.cs
+++ Assets/EmitOutsideCamera.cs
@@ -5,7 +5,7 @@ public class EmitOutsideCamera : MonoBehaviour
     [SerializeField] private SOEvent outsideCameraEvent;
     [SerializeField] private string tagToLookFor;
 
-    void OnTriggerExit2D(Collider2D col)
+    void OnTriggerEnter2D(Collider2D col)
     {
         if (!col.CompareTag(tagToLookFor))
             return;
